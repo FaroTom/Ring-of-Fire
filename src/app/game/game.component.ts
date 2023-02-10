45b8beb7ae0441ecadb9740f200280572f5582b4
Farotom <tom.krause-100@gmx.de>
2023-02-10 12:14:41 +0100

    added game page and card effects

---
+++ src/app/game/game.component.ts
@@ -0,0 +1,26 @@
+import { Component,OnInit } from '@angular/core';
+import { Game } from 'src/models/game';
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.scss']
+})
+export class GameComponent implements OnInit {
+  pickCardAnimation = false;
+  game: Game;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+  }
+
+  takeCard() {
+    this.pickCardAnimation = true;
+  }
+}
